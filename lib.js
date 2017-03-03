ae0db2b6d1eca05453517b8127d2451fc5f9f522
Anthony Pham <anthonytpham31@gmail.com>
2017-03-02 21:17:57 -0600

    Finished, little sloppy

--- lib.js
+++ lib.js
@@ -17,30 +17,45 @@ const customer =
     shoppingList
   })
 
-const cart =
-  (customer, ...items) => ({
-    customer,
-    items
-  })
+// const cart =
+//   (customer, ...items) => ({
+//     customer,
+//     items
+//   })
 
 /**
  * should return an array with the `itemName` repeated `count` number of times
  */
 const itemRepeater =
   itemName =>
-    count => {
-      // TODO
-    }
+    count => new Array(count).fill(itemName)
 
 /**
  * should return an array of carts with each given customer's shopping list
  * as an array of items
  */
+// entries(customer.shoppingList) displays [['hennessey', 1], ['carlos', 2]] etc.
+// iterate entries(customer.shoppinglist) over itemRepeater since there are possible multiple items in a cart
+
 const constructCarts =
   listings =>
     customers => {
-      // TODO
+      let arr = []
+      customers.forEach(customer =>
+        arr.push({
+          customer: customer.name,
+          items: entries(customer.shoppingList).map(
+            x => itemRepeater(x[0])(x[1])).reduce(
+            (p, c) => { return [...p, ...c] }, []
+          )
+        })
+      )
+      return arr
     }
+// const fullShoppingList = entries(customer.shoppingList)
+// const mapShoppingList = fullShoppingList.map(x => itemRepeater(x[0])(x[1]))
+// const arrayOfStrings = mapShoppingList.reduce((p, c) => { return [...p, ...c] }, [])
+// const customerName = customer.name
 
 module.exports = {
   listing,
